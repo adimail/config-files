e47cad659266a5da0d4fabe73c8e921fde2802f3
Aditya <adimail2404@gmail.com>
2023-12-31 01:01:02 +0530

    powershell update

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,27 +13,50 @@ Function whereis ($command) {
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-Function Set-ConsoleColor {
-    param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [System.ConsoleColor]$ForegroundColor,
 
-        [Parameter(Position = 1, Mandatory = $false)]
-        [System.ConsoleColor]$BackgroundColor = [System.Console]::BackgroundColor
-    )
-
-    $Host.UI.RawUI.ForegroundColor = $ForegroundColor
-    $Host.UI.RawUI.BackgroundColor = $BackgroundColor
-    Clear-Host
+#Opens a folder in my directory for practice coding
+Function classroom {
+    code C:\Users\pradi\Documents\classroom
 }
+
+function portfolio ($command){
+    if($command -eq "view"){
+        start https://adimail.github.io
+    } elseif ($command -eq "code"){
+        code C:\Users\pradi\Documents\adimail.github.io
+    } else {
+        echo "Invalid command: comands avaliable=>  "view" && "code""
+    }
+}
+
+function llm ($command){
+    if($command -eq "gpt"){
+        start https://chat.openai.com
+    } elseif ($command -eq "bard"){
+		start https://bard.google.com/chat
+    } else {
+        echo "Invalid command: comands avaliable=>  gpt && bard"
+    }
+}
+
+function yt ($command){
+    if ($command -eq "hist") {
+        start https://www.youtube.com/feed/history
+    } elseif (-not $command) {
+        start https://www.youtube.com
+    } else {
+        echo "Invalid command: commands available=> hist"
+    }
+}
+
 
 Function Get-InternetStatus {
     $network = Get-NetConnectionProfile
 
     if ($network) {
-        return "Connected to $($network.Name) - Internet is available"
+        return "Connected to $($network.Name) $($network.wlan)"
     } else {
-        return "Not connected to any network - No internet access"
+        return "Offline"
     }
 }
 
@@ -46,14 +69,39 @@ Function Show-StartupInfo {
 
 ****************************************************
 
-Current Path: $currentPath
-$internetStatus
+Current Path    : $currentPath
+Internet Status : $internetStatus
 
 ****************************************************
 
 "@
 }
 
+function getwifipasswords {
+    $wifiProfiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object {
+        $_ -replace "^\s+All User Profile\s+:\s+"
+    }
+
+    foreach ($profile in $wifiProfiles) {
+        $wifiInfo = netsh wlan show profile $profile key=clear
+        $wifiInfo | Select-String "SSID name", "Key Content"
+        Write-Host "------------------------"
+    }
+}
+
+
+function readwifinearme {
+    $wifiNetworks = netsh wlan show networks mode=Bssid
+    $wifiNetworks | Select-String "SSID", "Signal", "Channel", "Authentication", "Encryption"
+}
+
+
+[System.Console]::Title = "pwsh"
+
+
 # Call the function when a new terminal session starts
-figlet Welcome Adi
+figlet Welcome ADI
 Show-StartupInfo
+
+import-Module terminal-icons
+set-PSReadLineOption -PredictionViewStyle ListView
